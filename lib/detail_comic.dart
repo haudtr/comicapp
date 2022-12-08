e172b099c548971ebe1926805678662adaffba44
truongquochuy3001 <huy30012001@gmail.com>
2022-12-08 19:57:11 +0700

    lam comic detail

--- lib/detail_comic.dart
+++ lib/detail_comic.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class DetailComic extends StatefulWidget {
   const DetailComic({super.key});
@@ -7,7 +10,28 @@ class DetailComic extends StatefulWidget {
   State<DetailComic> createState() => _DetailComicState();
 }
 
+const double width = 300.0;
+const double height = 40.0;
+const double loginAlign = -1;
+const double signInAlign = 1;
+const Color selectedColor = Colors.white;
+const Color normalColor = Colors.black54;
+
 class _DetailComicState extends State<DetailComic> {
+  late double xAlign;
+  late Color loginColor;
+  late Color signInColor;
+
+  int selectedItem = 1;
+
+  @override
+  void initState() {
+    super.initState();
+    xAlign = loginAlign;
+    loginColor = selectedColor;
+    signInColor = normalColor;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,25 +61,32 @@ class _DetailComicState extends State<DetailComic> {
                 ],
               ),
             ),
-            Image.asset("assets/images/Rectangle 30.png"),
+            Image.asset("assets/images/image 5.png"),
+            const SizedBox(
+              height: 10,
+            ),
             Container(
-              padding: const EdgeInsets.only(left: 20, right: 20),
+              padding: const EdgeInsets.only(left: 30, right: 30),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   const SizedBox(
                     child: Text(
-                      "Komi san Cant Communicate",
-                      style: TextStyle(fontSize: 20),
+                      "Jujutsu kaisen",
+                      style: TextStyle(fontSize: 25),
                     ),
                   ),
                   ElevatedButton(
                       style: ElevatedButton.styleFrom(
                         backgroundColor:
                             const Color.fromARGB(255, 146, 198, 224),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25),
+                        ),
                       ),
                       onPressed: () {},
                       child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: const [
                           Icon(Icons.heart_broken),
                           Text("61"),
@@ -63,10 +94,269 @@ class _DetailComicState extends State<DetailComic> {
                       ))
                 ],
               ),
+            ),
+            // ToggleButtons(
+            //   borderRadius: BorderRadius.circular(20),
+            //   isSelected: _selections,
+            //   onPressed: (int index) {
+            //     setState(() {
+            //       _selections[index] = !_selections[index];
+            //     });
+            //   },
+            //   children: const [
+            //     Text("Mo ta"),
+            //     Text("Danh sach chuong"),
+            //   ],
+            // ),
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+              margin: const EdgeInsets.only(left: 20, right: 20),
+              width: MediaQuery.of(context).size.width,
+              height: 40,
+              decoration: BoxDecoration(
+                border:
+                    Border.all(color: const Color.fromARGB(255, 179, 189, 194)),
+                color: Colors.white,
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(50.0),
+                ),
+              ),
+              child: Stack(
+                children: [
+                  AnimatedAlign(
+                    alignment: Alignment(xAlign, 0),
+                    duration: const Duration(milliseconds: 400),
+                    child: Container(
+                      width: MediaQuery.of(context).size.width * 0.5,
+                      height: 40,
+                      decoration: const BoxDecoration(
+                        color: Colors.grey,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(50.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        xAlign = loginAlign;
+                        loginColor = selectedColor;
+
+                        signInColor = normalColor;
+                        selectedItem = 1;
+                      });
+                    },
+                    child: Align(
+                      alignment: const Alignment(-1, 0),
+                      child: Container(
+                        width: MediaQuery.of(context).size.width * 0.4,
+                        color: Colors.transparent,
+                        alignment: Alignment.center,
+                        child: Text(
+                          'Mo ta',
+                          style: TextStyle(
+                              color: loginColor,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 14),
+                        ),
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        xAlign = signInAlign;
+                        signInColor = selectedColor;
+
+                        loginColor = normalColor;
+                        selectedItem = 0;
+                      });
+                    },
+                    child: Align(
+                      alignment: const Alignment(1, 0),
+                      child: Container(
+                        width: MediaQuery.of(context).size.width * 0.4,
+                        color: Colors.transparent,
+                        alignment: Alignment.center,
+                        child: Text(
+                          'Danh sach chuong',
+                          style: TextStyle(
+                            color: signInColor,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Expanded(
+              child: selectedItem == 1 ? description() : chapList(),
             )
           ],
         ),
       ),
     );
+  }
+
+  description() {
+    return Padding(
+      padding: const EdgeInsets.only(left: 30, right: 30),
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [
+                Text(
+                  "Yuji",
+                  style: TextStyle(fontSize: 25),
+                ),
+                Text("main character"),
+              ],
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            const Text(
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 15, right: 15),
+              width: MediaQuery.of(context).size.width,
+              height: 70,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                border: Border.all(
+                  color: const Color.fromARGB(255, 146, 198, 224),
+                ),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  RatingBar(
+                    itemSize: 35,
+                    direction: Axis.horizontal,
+                    allowHalfRating: true,
+                    itemCount: 5,
+                    ratingWidget: RatingWidget(
+                      full: const Icon(
+                        Icons.star,
+                        color: Colors.blue,
+                      ),
+                      half: const Icon(
+                        Icons.star_half,
+                        color: Colors.blue,
+                      ),
+                      empty: const Icon(
+                        Icons.star_border,
+                        color: Colors.blue,
+                      ),
+                    ),
+                    itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+                    onRatingUpdate: (rating) {
+                      // print(rating);
+                    },
+                  ),
+                  const Divider(
+                    color: Colors.black,
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Text(
+                        "265",
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      Text("danh gia"),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  chapList() {
+    return Padding(
+        padding: const EdgeInsets.only(left: 30, right: 30),
+        child: Column(
+          children: [
+            const Text(
+              "88 chuong",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    for (int i = 0; i < 88; i++)
+                      Column(
+                        children: [
+                          SizedBox(
+                            height: 5,
+                          ),
+                          GestureDetector(
+                            onTap: () {},
+                            child: Row(
+                              children: [
+                                const Text(
+                                  "chuong 1",
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                Expanded(
+                                    child: Container(
+                                  width: MediaQuery.of(context).size.width,
+                                  padding: EdgeInsets.only(left: 10),
+                                  decoration: BoxDecoration(color: Colors.grey),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: const [
+                                      Text(
+                                        "giao dich",
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                      Text(
+                                        "1 ngay truoc - 12 view",
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ],
+                                  ),
+                                ))
+                              ],
+                            ),
+                          )
+                        ],
+                      )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ));
   }
 }
